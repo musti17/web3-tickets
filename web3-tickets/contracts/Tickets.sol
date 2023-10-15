d8dc647d49c2d0e6c759d7fce85debbf9f4177a5
Mustafa Zahid <mustafazahid167@gmail.com>
2023-10-15 12:23:58 +0500

    BuyTicket functionality added to the contract

--- web3-tickets/contracts/Tickets.sol
+++ web3-tickets/contracts/Tickets.sol
@@ -1,4 +1,6 @@
-pragma solidity >=0.4.22 <0.90;
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.4.22 <0.9.0;
 
 uint constant Total_Tickets = 10;
 
@@ -18,5 +20,12 @@ contract Tickets{
             tickets[i].price = 1e17; // 0.1 ETH
             tickets[i].owner = address(0x0);
         }
+    }
+
+    function buyTicket(uint256 _index) external payable{
+        require(_index >= 0 && _index < Total_Tickets); //make sure that tickets id is btw 0-9
+        require(tickets[_index].owner == address(0x0));//make sure that ticket is not owned by anyone
+        require(msg.value >= tickets[_index].price);//make sure that enough money sent to function
+        tickets[_index].owner = msg.sender;
     }
 }
