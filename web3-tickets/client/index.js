bfea2c7632292eff31ce1a8a512168bbb46f867b
Mustafa Zahid <mustafazahid167@gmail.com>
2023-10-16 17:27:40 +0500

    Code written to request access to user's Ethereum accounts (wallet addresses) using MetaMask or the connected Ethereum provider. Displays the user's Ethereum account address in an HTML element with the ID 'account' on the web page.

--- web3-tickets/client/index.js
+++ web3-tickets/client/index.js
@@ -6,8 +6,25 @@ import configuration from '../build/contracts/Tickets.json';
 const Contract_Address = configuration.networks['5777'].address;
 const Contract_ABI = configuration.abi;
 
-const web3 = new Web3(
-    Web3.givenProvider || 'http://127.0.0.1:7545'
-);
+// Create a Web3 instance using the injected provider (e.g., MetaMask)
+const web3 = new Web3(window.ethereum || 'http://127.0.0.1:7545');
 
-const contract = new web3.eth.Contract(Contract_ABI,Contract_Address);
+const contract = new web3.eth.Contract(Contract_ABI, Contract_Address);
+
+let account;
+
+const accountEl = document.getElementById('account');
+
+const main = async () => {
+    try {
+        // Request access to user accounts using the new method
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const accounts = await web3.eth.getAccounts();
+        account = accounts[0];
+        accountEl.innerText = account;
+    } catch (error) {
+        console.error('Error requesting accounts:', error);
+    }
+};
+
+main();
