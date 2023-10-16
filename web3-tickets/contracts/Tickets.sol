9da5c5f5b64434ed83f67f954665d43724852b43
Mustafa Zahid <mustafazahid167@gmail.com>
2023-10-16 12:14:55 +0500

    Migrations code added to deploy the smart contract to the local etheruem blockchain Note:Check solidity version in truffle-config.js as it causes issues when compiling and deploying contracts

--- web3-tickets/contracts/Tickets.sol
+++ web3-tickets/contracts/Tickets.sol
@@ -1,31 +1,30 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 
 pragma solidity >=0.4.22 <0.9.0;
 
-uint constant Total_Tickets = 10;
+uint256 constant TOTAL_TICKETS = 10;
 
-contract Tickets{
-    address public owner = msg.sender;
+contract Tickets {
+  address public owner = msg.sender;
 
-    struct Ticket{
-        uint256 price;
-        address owner;
+  struct Ticket {
+    uint256 price;
+    address owner;
+  }
+
+  Ticket[TOTAL_TICKETS] public tickets;
+
+  constructor() {
+    for (uint256 i = 0; i < TOTAL_TICKETS; i++) {
+      tickets[i].price = 1e17; // 0.1 ETH
+      tickets[i].owner = address(0x0);
     }
+  }
 
-    Ticket[Total_Tickets] public tickets;
-
-    constructor(){
-        for(uint256 i = 0; i < Total_Tickets; i++)
-        {
-            tickets[i].price = 1e17; // 0.1 ETH
-            tickets[i].owner = address(0x0);
-        }
-    }
-
-    function buyTicket(uint256 _index) external payable{
-        require(_index >= 0 && _index < Total_Tickets); //make sure that tickets id is btw 0-9
-        require(tickets[_index].owner == address(0x0));//make sure that ticket is not owned by anyone
-        require(msg.value >= tickets[_index].price);//make sure that enough money sent to function
-        tickets[_index].owner = msg.sender;
-    }
+  function buyTicket(uint256 _index) external payable {
+    require(_index < TOTAL_TICKETS && _index >= 0);
+    require(tickets[_index].owner == address(0x0));
+    require(msg.value >= tickets[_index].price);
+    tickets[_index].owner = msg.sender;
+  }
 }
