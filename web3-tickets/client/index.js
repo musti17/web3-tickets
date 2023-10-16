564ca840441eba5bef2ac6eade5f166e5d415e08
Mustafa Zahid <mustafazahid167@gmail.com>
2023-10-16 16:32:32 +0500

    *Json of smart contract imported *Address and abi of smart contract retrieved *New web3 instance created to interact with the Etheruem network *Contract object created using the instance,abi and address to interact with the deployed smart contract on Ethereum Blockchain

--- web3-tickets/client/index.js
+++ web3-tickets/client/index.js
@@ -1,2 +1,13 @@
 import Web3 from 'web3';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import configuration from '../build/contracts/Tickets.json';
+
+const Contract_Address = configuration.networks['5777'].address;
+const Contract_ABI = configuration.abi;
+
+const web3 = new Web3(
+    Web3.givenProvider || 'http://127.0.0.1:7545'
+);
+
+const contract = new web3.eth.Contract(Contract_ABI,Contract_Address);
