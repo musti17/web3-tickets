049078d76a46452ba649c1308c9c134b22b18b6a
Mustafa Zahid <mustafazahid167@gmail.com>
2023-10-18 21:58:33 +0500

    Populating/Rendering the ticketEl element with information about each ticket in my smart contract alongside the image

--- web3-tickets/client/index.js
+++ web3-tickets/client/index.js
@@ -15,13 +15,39 @@ const Contract_Address = configuration.networks['5777'].address;
 const Contract_ABI = configuration.abi;
 
 // Create a Web3 instance using the injected provider (e.g., MetaMask)
-const web3 = new Web3(window.ethereum || 'http://127.0.0.1:7545');
+const web3 = new Web3(Web3.givenProvider || 'http://127.0.0.1:7545');
 
 const contract = new web3.eth.Contract(Contract_ABI, Contract_Address);
 
+const TOTAL_TICKETS = 10;
 let account;
 
+const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
+
 const accountEl = document.getElementById('account');
+const ticketsEl = document.getElementById('tickets');
+
+const refreshTickets = async() => {
+    ticketsEl.innerHTML = '';
+    for(let i = 0; i < TOTAL_TICKETS; i++){
+        const ticket = await contract.methods.tickets(i).call();
+        ticket.id = i;
+        if(ticket.owner === EMPTY_ADDRESS)
+        {
+            const ticketEl = createElementFromString(
+                `<div class="card" style="width: 18rem;">
+                <img src="${ticketImage}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">${ticket.price}</p>
+                  <button class="btn btn-primary">Buy</button>
+                </div>
+              </div>`
+            );
+            ticketsEl.appendChild(ticketEl);
+        }
+    }
+}
 
 const main = async () => {
     try {
@@ -30,6 +56,7 @@ const main = async () => {
         const accounts = await web3.eth.getAccounts();
         account = accounts[0];
         accountEl.innerText = account;
+        await refreshTickets();
     } catch (error) {
         console.error('Error requesting accounts:', error);
     }
